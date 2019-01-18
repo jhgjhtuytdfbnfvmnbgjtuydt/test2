ecc4549ae06154ca26bdc93ba70aee7696b1dc38
julian <julian.romera@gmail.com>
2019-01-18 19:47:08 +0100

    correct bugs. add management console

--- src/main/java/es/santalucia/api/ApiApplication.java
+++ src/main/java/es/santalucia/api/ApiApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @EnableConfigurationProperties
+// @EntityScan(basePackages = { "es.santalucia.api.model" }) // scan JPA entities
 public class ApiApplication {
 
 	public static void main(String[] args) {
