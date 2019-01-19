fbbd88e9020f8fee79547815fe11239bdc0d9142
julian <julian.romera@gmail.com>
2019-01-19 18:26:23 +0100

    generate initial database schema. update app properties. add packages. add livereaload on changes classes

--- src/main/java/es/santalucia/api/ApiApplication.java
+++ src/main/java/es/santalucia/api/ApiApplication.java
@@ -2,10 +2,12 @@ package es.santalucia.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-// @EnableConfigurationProperties
-// @EntityScan(basePackages = { "es.santalucia.api.model" }) // scan JPA entities
+@EnableConfigurationProperties
+@EntityScan(basePackages = { "es.santalucia.model" }) // scan JPA entities
 public class ApiApplication {
 
 	public static void main(String[] args) {
